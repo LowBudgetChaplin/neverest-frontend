6abe55c2f9df99a36893c769d51246a107226af8
Serban Corodescu <serbancorodescu14@gmail.com>
2025-10-05 20:11:35 +0300

    flutter upgrade + folders hierarchy

---
+++ lib/app/router/app_router.dart
@@ -0,0 +1,9 @@
+import 'package:go_router/go_router.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+const login = "/login";
+const home = "/home";
+
+final appRouterProvider = Provider<GoRouter>((ref) {
+    //todo: routers
+});
